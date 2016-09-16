4d312b54ac21f19f55a29cadd3fff7bdb0f55099
Jordan Smith <jtsu888@gmail.com>
2016-09-16 13:00:47 -0700

    Use ZDCSessionConfig

--- ios/RNZendeskChatModule.m
+++ ios/RNZendeskChatModule.m
@@ -32,7 +32,7 @@ RCT_EXPORT_METHOD(startChat:(NSDictionary *)options) {
   [self setVisitorInfo:options];
 
   dispatch_sync(dispatch_get_main_queue(), ^{
-    [ZDCChat startChat:^(ZDCConfig *config) {
+    [ZDCChat startChat:^(ZDCSessionConfig *config) {
       config.preChatDataRequirements.name       = ZDCPreChatDataRequired;
       config.preChatDataRequirements.email      = ZDCPreChatDataRequired;
       config.preChatDataRequirements.phone      = ZDCPreChatDataRequired;
